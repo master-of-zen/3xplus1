c42d9d78a563bb437b0c8be2434c1222cd3c7b21
Zen <46526140+master-of-zen@users.noreply.github.com>
2021-07-30 00:25:13 +0300

    parallelixm, that alocates ~10 GB

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
+use rayon::prelude::*;
 fn main() {
     println!("Let's gooooo");
 
-    let mut i = 1usize;
-    let mut start = 1usize;
-    let mut iterations = 0usize;
+    let p: Vec<(u64, u64)> = (1..600000000u64)
+        .into_par_iter()
+        .map(|x| calc_loop(x))
+        .collect();
+
+    println!("{:#?}", &p.len());
+}
+
+fn calc_loop(start_number: u64) -> (u64, u64) {
+    let mut i = start_number;
+    let mut iterations = 0u64;
+
     loop {
         iterations += 1;
         if i % 2 == 0 {
@@ -13,10 +23,7 @@ fn main() {
         }
 
         if i == 1 {
-            println!("{} reached in {} iterations", start, iterations);
-            iterations = 0;
-            start += 1;
-            i = start;
+            return (start_number, iterations);
         }
     }
 }
