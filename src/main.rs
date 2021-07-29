f51d470c88ddc1c59187dc5a250a3a0aa13f9aae
Zen <46526140+master-of-zen@users.noreply.github.com>
2021-07-29 20:01:32 +0300

    code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Let's gooooo");
+
+    let mut i = 1usize;
+    let mut start = 1usize;
+    let mut iterations = 0usize;
+    loop {
+        iterations += 1;
+        if i % 2 == 0 {
+            i /= 2
+        } else {
+            i = 3 * i + 1;
+        }
+
+        if i == 1 {
+            println!("{} reached in {} iterations", start, iterations);
+            iterations = 0;
+            start += 1;
+            i = start;
+        }
+    }
 }
