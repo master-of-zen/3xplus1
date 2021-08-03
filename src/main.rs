1662da03bdab4ffa2579753bc01f1e3e327ca6cd
Zen <46526140+master-of-zen@users.noreply.github.com>
2021-08-03 08:53:49 +0300

    new impementation + optimizations

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,42 @@ use std::time::{Duration, Instant};
 fn main() {
     println!("Let's gooooo");
     let now = Instant::now();
-    par_calculation();
+    // x
+    //par_calculation();
+
+    // 19.524
+    functional();
     println!("Done: {}", now.elapsed().as_millis());
 }
 
 fn par_calculation() {
     let mut biggest: (u32, u32) = (0, 0);
 
-    for i in (1..1000000000).step_by(100000) {
-        let arr: [u32; 100000] = (i as u32..(i as u32 + 100000u32))
+    for i in (1..1000000000).step_by(1000000) {
+        let arr: [u32; 1000000] = (i as u32..(i as u32 + 1000000u32))
             .collect::<Vec<u32>>()
             .try_into()
             .expect("wrong size iterator");
         let res = process_nums(&arr, biggest.1);
         for i in res.iter() {
+            if i.1 > biggest.1 {
+                println!("{}:{}", i.0, i.1);
+                biggest = (i.0, i.1);
+            }
+        }
+    }
+}
+
+fn functional() {
+    let mut biggest: (u32, u32) = (0, 0);
+    for i in (1..1000000000u32).step_by(1000000) {
+        let vc: Vec<(u32, u32)> = (i as u32..(i as u32 + 1000000u32))
+            .into_par_iter()
+            .map(|x| (x, process_u32(x)))
+            .filter(|x| x.1 > biggest.1)
+            .collect::<Vec<(u32, u32)>>();
+
+        for i in vc.iter() {
             if i.1 > biggest.1 {
                 println!("{}:{}", i.0, i.1);
                 biggest = (i.0, i.1);
@@ -32,8 +54,8 @@ fn process_u32(start_number: u32) -> u32 {
 
     loop {
         iterations += 1;
-        if i % 2 == 0 {
-            i /= 2
+        if i & 1 == 0 {
+            i /= 2;
         } else {
             i = 3 * i + 1;
         }
