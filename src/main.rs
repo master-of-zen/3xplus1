880de5406f29ed3e30cac9695c98aedcae9ee2de
Zen <46526140+master-of-zen@users.noreply.github.com>
2021-08-03 07:24:38 +0300

    better parallelizing

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,34 @@
 use rayon::prelude::*;
 use std::convert::TryInto;
+use std::time::{Duration, Instant};
 fn main() {
     println!("Let's gooooo");
-
+    let now = Instant::now();
     par_calculation();
+    println!("Done: {}", now.elapsed().as_millis());
 }
 
 fn par_calculation() {
-    let mut biggest: (u128, u128) = (0, 0);
+    let mut biggest: (u32, u32) = (0, 0);
 
-    for i in (1..u64::MAX).step_by(1000) {
-        let arr: [u128; 1000] = (i as u128..(i as u128 + 1000u128))
-            .collect::<Vec<u128>>()
+    for i in (1..1000000000).step_by(100000) {
+        let arr: [u32; 100000] = (i as u32..(i as u32 + 100000u32))
+            .collect::<Vec<u32>>()
             .try_into()
             .expect("wrong size iterator");
-        let res = process_nums(&arr);
-        if res.1 > biggest.1 {
-            println!("{}:{}", res.0, res.1);
-            biggest = res;
+        let res = process_nums(&arr, biggest.1);
+        for i in res.iter() {
+            if i.1 > biggest.1 {
+                println!("{}:{}", i.0, i.1);
+                biggest = (i.0, i.1);
+            }
         }
     }
 }
 
-fn process_u128(start_number: u128) -> u128 {
+fn process_u32(start_number: u32) -> u32 {
     let mut i = start_number;
-    let mut iterations = 0u128;
+    let mut iterations = 0u32;
 
     loop {
         iterations += 1;
@@ -40,14 +44,11 @@ fn process_u128(start_number: u128) -> u128 {
     }
 }
 
-fn process_nums(input: &[u128]) -> (u128, u128) {
-    let mut comb: Vec<(u128, u128)> = input
-        .clone()
+fn process_nums(input: &[u32], biggest: u32) -> Vec<(u32, u32)> {
+    let comb: Vec<(u32, u32)> = input
         .par_iter()
-        .map(|&i| (i, process_u128(i)))
+        .map(|&i| (i, process_u32(i)))
+        .filter(|x| x.1 > biggest)
         .collect();
-
-    comb.sort_by(|x, y| x.1.cmp(&y.1));
-
-    comb[0]
+    comb
 }
